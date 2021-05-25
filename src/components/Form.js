58e244ece4f64fd262f9c6de3f1d04185c0befa7
Zohaib Sohail <zohaibsohail@Zohaibs-MacBook-Air.local>
2021-05-25 20:41:06 +0500

    confirm password validation added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,27 @@
-import * as React from "react";
+import React, {useRef} from "react";
 import { useForm } from "react-hook-form";
 
 export default function Form({inputs, button, onSubmit}) {
-  const { register, handleSubmit, formState: { errors }  } = useForm();
+  const { register, handleSubmit, watch,  formState: { errors }  } = useForm();
+  const password = useRef({}); 
+  password.current = watch("password", "");
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
         
     {
         inputs.map((inp, index) => <div key={index}>
-                <input {...register(inp.name, {required: true, pattern: inp.name === 'email' ? /^\S+@\S+$/i : '' })} placeholder={inp.placeholder} type={inp.type}/>
+                <input {...register(inp.name, {
+                        ...(inp.name === 'confirmPassword' && {validate: value => value === password.current || "The passwords do not match"}),
+                        required: true,
+                        ...(inp.name === 'email' && {pattern: /^\S+@\S+$/i})
+                     })}
+                        placeholder={inp.placeholder}
+                        type={inp.type}/>
                 <br/>
-                {errors[inp.name] && errors[inp.name].type === "required" && <span>This is required</span>}
-                {errors[inp.name] && inp.name === 'email' && errors[inp.name].type === "pattern" && <span>Not a valid Email</span> }
+                {errors[inp.name] && errors[inp.name].type === "required" && <span style={{color: 'red', fontSize:'10px'}}>This is required</span>}
+                {errors[inp.name] && inp.name === 'email' && errors[inp.name].type === "pattern" && <span style={{color: 'red', fontSize:'10px'}}>Not a valid Email</span> }
+                {errors[inp.name] && inp.name === 'confirmPassword' && errors[inp.name].type === "validate" && <span style={{color: 'red', fontSize:'10px'}}>{errors[inp.name].message}</span> }
                 <br/>
                 <br/>
             </div>
